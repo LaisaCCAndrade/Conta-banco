85c675e7d194425f5ddde29f20cc7b116c3d0140
Laisa <laisacristina2606@gmail.com>
2023-08-23 09:03:09 -0300

    conta criada

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -4,11 +4,10 @@ import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         // TODO: Conhecer e importar a classe Scanner
-
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         // Exibir as mensagens para o nosso usuário
-
+        // Obter pela scanner os valores digitados no terminal
         System.out.println("Programa: Por favor, digite seu nome:");
         String nomeCliente = scanner.next();
 
@@ -21,10 +20,7 @@ public class ContaTerminal {
         System.out.println("Seu saldo:");
         double saldo = scanner.nextDouble();
 
-        // Obter pela scanner os valores digitados no terminal
-
         // Exibir a mensagem conta criada
-
-        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é  " + agencia + ", conta " + numero + " e seu saldo de R$ " + saldo + " já está disponível para saque");
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é  " + agencia + ", conta " + numero + " e seu saldo de R$ " + saldo + " já está disponível para saque!");
     }
 }
